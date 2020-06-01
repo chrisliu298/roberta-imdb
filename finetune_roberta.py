1aec36b3e28a7c10fd5af35f6860bde26aa5deb6
convexlifefunction <yuhaoliu298@gmail.com>
2020-05-31 19:45:24 -0700

    Reformat code

--- finetune_roberta.py
+++ finetune_roberta.py
@@ -3,6 +3,7 @@ import datetime
 import random
 import numpy as np
 import pandas as pd
+import sys
 import torch
 from torch.utils.data import TensorDataset, random_split
 from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
@@ -13,15 +14,17 @@ from transformers import (
 )
 from transformers import AdamW
 from transformers import get_linear_schedule_with_warmup
+from tqdm import tqdm
 
 
 # Parameters
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 max_len = 512
-batch_size = 4
-epochs = 3
-learning_rate = 1e-5
+batch_size = 1
+epochs = 5
+learning_rate = 1e-6
 seed = 42
+model_name = "distilroberta-base"
 
 # Settings
 random.seed(seed)
@@ -47,148 +50,201 @@ def flat_accuracy(preds, labels):
     return np.sum(pred_flat == labels_flat) / len(labels_flat)
 
 
-# Read data
-df = pd.read_csv("labeledTrainData.tsv", delimiter="\t")
-imdb_reviews = df["review"]
-sentiments = df["sentiment"]
+if __name__ == "__main__":
+    # Read data
+    train = pd.read_csv("labeledTrainData.tsv", delimiter="\t")
+    imdb_reviews = train["review"]
+    sentiments = train["sentiment"]
 
-# Tokenize the text
-tokenizer = RobertaTokenizer.from_pretrained("roberta-large", do_lower_case=True)
+    # Tokenize the text
+    tokenizer = RobertaTokenizer.from_pretrained(model_name, do_lower_case=True)
 
-input_ids = []
-attention_masks = []
+    input_ids = []
+    attention_masks = []
 
-for review in imdb_reviews:
-    encoded_dict = tokenizer.encode_plus(
-        review,
-        add_special_tokens=True,
-        max_length=max_len,
-        pad_to_max_length=True,
-        return_attention_mask=True,
-        return_tensors="pt",
-    )
-    input_ids.append(encoded_dict["input_ids"])
-    attention_masks.append(encoded_dict["attention_mask"])
-
-input_ids = torch.cat(input_ids, dim=0)
-attention_masks = torch.cat(attention_masks, dim=0)
-labels = torch.tensor(sentiments)
-
-# Make data loader
-dataset = TensorDataset(input_ids, attention_masks, labels)
-train_size = int(0.9 * len(dataset))
-valid_size = len(dataset) - train_size
-train_dataset, valid_dataset = random_split(datset, split_ratio=[train_size, valid_size])
-
-print("{:>5,} training samples".format(train_size))
-print("{:>5,} validation samples".format(val_size))
-
-train_dataloader = DataLoader(
-    train_dataset, sampler=RandomSampler(train_dataset), batch_size=batch_size
-)
-
-valid_dataloader = DataLoader(
-    valid_dataset, sampler=SequentialSampler(valid_dataset), batch_size=batch_size
-)
-
-# Import the model
-model = RobertaForSequentialClassification.from_pretrained(
-    "roberta-large", num_labels=2, output_attentions=False, output_hidden_states=False
-)
-model.to(device)
-
-optimizer = AdamW(model.parameter(), le=learning_rate, ops=1e-8)
-
-total_steps = len(train_dataloader) * epochs
-scheduler = get_linear_schedule_with_warmup(
-    optimizer, num_training_steps=total_steps, num_warmup_steps=0
-)
-
-
-# Finetune the model
-train_stats = []
-total_time = time.time()
-
-for epoch in range(0, epochs):
-    print("=" * 10 + f" Epoch {epoch}/{epochs} " + "=" * 10)
-
-    # Train
-    print("Training started ...")
-    now = time.time()
-    epoch_training_loss = 0
-    model.train()  # train mode
-    for step, batch in enumerate(train_dataloader):
-        if step % 50 == 0 and not step == 0:
-            elapsed = format_time(time.time() - now)
-            print(
-                "Batch {:>5,} of {:>5,}.  Elapsed: {:}.".format(
-                    step, len(train_dataloader), elapsed
-                )
-            )
-        batch_input_ids = batch[0].to(device)
-        batch_attention_masks = batch[1].to(device)
-        batch_labels = batch[2].to(device)
-        model.zero_grad()
-        loss, logits = model(
-            input_ids=batch_input_ids,
-            attention_mask=batch_attention_masks,
-            labels=batch_labels,
+    for review in imdb_reviews:
+        encoded_dict = tokenizer.encode_plus(
+            review,
+            add_special_tokens=True,
+            max_length=max_len,
+            pad_to_max_length=True,
+            return_attention_mask=True,
+            return_tensors="pt",
         )
-        epoch_training_loss += loss.item()
-        loss.backward()
-        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
-        optimizer.step()
-        scheduler.step()
+        input_ids.append(encoded_dict["input_ids"])
+        attention_masks.append(encoded_dict["attention_mask"])
 
-    avg_train_loss = epoch_training_loss / len(train_dataloader)
-    train_time = format_time(time.time() - now)
-    print("\nAverage train loss: {0:.2f}".format(avg_train_loss))
-    print("Training epcoh took: {:}".format(train_time))
+    input_ids = torch.cat(input_ids, dim=0)
+    attention_masks = torch.cat(attention_masks, dim=0)
+    labels = torch.tensor(sentiments)
 
-    # Validation
-    print("\nValidation started ...")
-    now = time.time()
-    model.eval()  # evaluation mode
-    epoch_eval_acc = 0
-    epochs_eval_loss = 0
-    eval_steps = 0
+    # Make data loader
+    dataset = TensorDataset(input_ids, attention_masks, labels)
+    train_size = int(0.90 * len(dataset))
+    valid_size = len(dataset) - train_size
+    train_dataset, valid_dataset = random_split(dataset, [train_size, valid_size])
 
-    for batch in valid_dataloader:
-        batch_input_ids = batch[0].to(device)
-        batch_attention_masks = batch[1].to(device)
-        batch_labels = batch[2].to(device)
-        with torch.no_grad():
+    print("{:>5,} training samples".format(train_size))
+    print("{:>5,} validation samples".format(valid_size))
+
+    train_dataloader = DataLoader(
+        train_dataset, sampler=RandomSampler(train_dataset), batch_size=batch_size
+    )
+
+    valid_dataloader = DataLoader(
+        valid_dataset, sampler=SequentialSampler(valid_dataset), batch_size=batch_size
+    )
+
+    # Import the model
+    model = RobertaForSequenceClassification.from_pretrained(
+        model_name, num_labels=2, output_attentions=False, output_hidden_states=False
+    )
+    model.to(device)
+
+    optimizer = AdamW(model.parameters(), lr=learning_rate, eps=1e-8)
+
+    total_steps = len(train_dataloader) * epochs
+    scheduler = get_linear_schedule_with_warmup(
+        optimizer, num_training_steps=total_steps, num_warmup_steps=0
+    )
+
+    # Finetune the model
+    train_stats = []
+    total_time = time.time()
+
+    for epoch in range(0, epochs):
+        print(f"========== Epochs {epoch + 1} ==========")
+        # Train
+        now = time.time()
+        epoch_training_loss = 0
+        model.train()  # train mode
+        for step, batch in enumerate(
+            tqdm(train_dataloader, position=0, file=sys.stdout, leave=True)
+        ):
+            batch_input_ids = batch[0].to(device)
+            batch_attention_masks = batch[1].to(device)
+            batch_labels = batch[2].to(device)
+            model.zero_grad()
             loss, logits = model(
                 input_ids=batch_input_ids,
-                attention_masks=batch_attention_masks,
+                attention_mask=batch_attention_masks,
                 labels=batch_labels,
             )
-        epochs_eval_loss += loss.item()
+            epoch_training_loss += loss.item()
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+            optimizer.step()
+            scheduler.step()
+
+        avg_train_loss = epoch_training_loss / len(train_dataloader)
+        train_time = format_time(time.time() - now)
+        print("\nTrain loss: {0:.4f}".format(avg_train_loss))
+        print("Training time: {:}".format(train_time))
+
+        now = time.time()
+        model.eval()
+        epoch_eval_acc = 0
+        epochs_eval_loss = 0
+        eval_steps = 0
+
+        for batch in tqdm(valid_dataloader, position=0, file=sys.stdout, leave=True):
+            batch_input_ids = batch[0].to(device)
+            batch_attention_masks = batch[1].to(device)
+            batch_labels = batch[2].to(device)
+            with torch.no_grad():
+                loss, logits = model(
+                    input_ids=batch_input_ids,
+                    attention_mask=batch_attention_masks,
+                    labels=batch_labels,
+                )
+            epochs_eval_loss += loss.item()
+            logits = logits.detach().cpu().numpy()
+            labels_ids = batch_labels.to("cpu").numpy()
+            epoch_eval_acc += flat_accuracy(logits, labels_ids)
+
+        avg_valid_acc = epoch_eval_acc / len(valid_dataloader)
+        avg_valid_loss = epochs_eval_loss / len(valid_dataloader)
+        print()
+        print("Validation accuracy: {0:.4f}".format(avg_valid_acc))
+        valid_time = format_time(time.time() - now)
+        print("Validation loss: {0:.4f}".format(avg_valid_loss))
+        print("Validation time: {:}".format(valid_time))
+
+        train_stats.append(
+            {
+                "Epoch": epoch + 1,
+                "Train loss": avg_train_loss,
+                "Valid loss": avg_valid_loss,
+                "Valid acc": avg_valid_acc,
+                "Train Time": train_time,
+                "Validation Time": valid_time,
+            }
+        )
+
+        print()
+        print("Epoch completed.")
+        print(
+            "Total training took {:} (hh:mm:ss)".format(
+                format_time(time.time() - total_time)
+            )
+        )
+        print()
+
+    test = pd.read_csv("testData.tsv", delimiter="\t")
+    print("Number of test sentences: {:,}\n".format(test.shape[0]))
+    reviews = test["review"]
+    input_ids = []
+    attention_masks = []
+
+    for r in reviews:
+        encoded_dict = tokenizer.encode_plus(
+            r,
+            add_special_tokens=True,
+            max_length=max_len,
+            pad_to_max_length=True,
+            return_attention_mask=True,
+            return_tensors="pt",
+        )
+        input_ids.append(encoded_dict["input_ids"])
+        attention_masks.append(encoded_dict["attention_mask"])
+
+    input_ids = torch.cat(input_ids, dim=0)
+    attention_masks = torch.cat(attention_masks, dim=0)
+    labels = torch.tensor(labels)
+
+    batch_size = 64
+
+    prediction_data = TensorDataset(input_ids, attention_masks, labels)
+    prediction_sampler = SequentialSampler(prediction_data)
+    prediction_dataloader = DataLoader(
+        prediction_data, sampler=prediction_sampler, batch_size=batch_size
+    )
+
+    model.eval()
+
+    predictions, true_labels = [], []
+
+    for batch in prediction_dataloader:
+        batch = tuple(t.to(device) for t in batch)
+        b_input_ids, b_input_mask, b_labels = batch
+        with torch.no_grad():
+            outputs = model(
+                b_input_ids, token_type_ids=None, attention_mask=b_input_mask
+            )
+        logits = outputs[0]
         logits = logits.detach().cpu().numpy()
-        labels_ids = batch_labels.to("cpu").numpy()
-        epoch_eval_acc += flat_accuracy(logits, labels_ids)
+        label_ids = b_labels.to("cpu").numpy()
+        predictions.append(logits)
+        true_labels.append(label_ids)
 
-    avg_valid_acc = epoch_eval_acc / len(valid_dataloader)
-    avg_valid_loss = epochs_eval_loss / len(valid_dataloader)
-    print("Accuracy: {0:.2f}".format(avg_valid_acc))
+    pred = []
+    for i in predictions:
+        pred += list(np.argmax(i, axis=1))
 
-    valid_time = format_time(time.time() - now)
-    print("Validation Loss: {0:.2f}".format(avg_valid_loss))
-    print("Validation took: {:}".format(valid_time))
+    data = {"id": df["id"], "sentiment": pred}
+    submission = pd.DataFrame(data)
 
-    train_stats.append(
-        {
-            "epoch": epoch + 1,
-            "Train loss": avg_train_loss,
-            "Valid loss": avg_valid_loss,
-            "Valid acc": avg_valid_acc,
-            "Train Time": train_time,
-            "Validation Time": valid_time,
-        }
-    )
+    submission.to_csv("submission.csv", index=False)
 
-    print()
-    print("Finetuning completed.")
-    print(
-        "Total training took {:} (h:mm:ss)".format(format_time(time.time() - total_time))
-    )
+    model.save_pretrained("/content/drive/My Drive/models/roberta")
+    tokenizer.save_pretrained("/content/drive/My Drive/models/roberta")
